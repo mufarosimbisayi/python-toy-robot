522244e884c32ff39cc4ef1a9a0dd2d40fcf0a51
LMS Admin User <lms_admin@wethinkcode.co.za>
2021-08-12 16:53:59 +0000

    Submission Repo Created

---
+++ maze/obstacles.py
@@ -0,0 +1,19 @@
+# -------------------------------------------------------------------------------------------------
+#
+# TODO: Please replace this placeholder code with your solution for Toy Robot 4, and work from there.
+#
+# -------------------------------------------------------------------------------------------------
+
+def create_random_obstacles():
+    pass
+
+
+def is_position_blocked(x, y):
+    pass
+
+
+def is_path_blocked(x1, y1, x2, y2):
+    pass
+
+def get_obstacles():
+    pass
---
+++ robot.py
@@ -0,0 +1,15 @@
+# -------------------------------------------------------------------------------------------------
+#
+# TODO: Please replace this placeholder code with your solution for Toy Robot 4, and work from there.
+#
+# -------------------------------------------------------------------------------------------------
+
+def robot_start():
+    """
+    Replace me!
+    """
+    pass
+
+
+if __name__ == "__main__":
+    robot_start()
